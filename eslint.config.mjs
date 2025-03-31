f0d6d775b5e78b85a9d14e78f5fdae261e62b4e5
Anirudh Nair <anirudh.nair@simformsolutions.com>
2025-03-31 16:13:56 +0530

    fix: Fix eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,21 @@
-// @ts-check
-
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import prettierConfig from "eslint-config-prettier";
 export default tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.strictTypeChecked,
-    prettierConfig
+    prettierConfig,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+
+    {
+        files: ["**/*.mjs"],
+        extends: [tseslint.configs.disableTypeChecked],
+    }
 );
